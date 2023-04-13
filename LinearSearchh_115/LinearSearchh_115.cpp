323110923ee0bd7398b0387cd9d5ceb1cbe685b9
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-04-13 15:12:52 +0700

    langkah no 5 algoritma

--- LinearSearchh_115/LinearSearchh_115.cpp
+++ LinearSearchh_115/LinearSearchh_115.cpp
@@ -45,3 +45,10 @@ void LinearSearch()
 		ctr = 0;
 		for (i = 0; i < n; i++)								//langkah no 2 & 3 & 4
 		{
+			ctr++;
+			if (arr[i] == item)								//langkah no 5 algoritma
+			{
+				cout << "\n" << item << "found at position" << (i + 1) << endl;
+				break;
+			}
+		}
